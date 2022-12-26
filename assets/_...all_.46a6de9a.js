9df644a1de946f0896ee6913570edf983689372a
reqelp <reqelp@gmail.com>
2022-12-26 22:33:26 +0800

    delete

---
+++ assets/_...all_.46a6de9a.js
@@ -0,0 +1,1 @@
+import{d as e,u as n,b as s,t as a,f as r,o as c}from"./app.0349bd01.js";import{b as t}from"./route-block.023af118.js";const f={m:"2",text:"xl"},i=e({__name:"[...all]",setup(p){const{t:o}=n();return(_,l)=>(c(),s("div",f,a(r(o)("not-found")),1))}});typeof t=="function"&&t(i);export{i as default};
---
+++ assets/post.23931a7f.js
@@ -0,0 +1,1 @@
+import{_ as c,c as n,o as t,$ as _}from"./app.0349bd01.js";const e={};function s(r,a){const o=_;return t(),n(o)}const f=c(e,[["render",s]]);export{f as default};
